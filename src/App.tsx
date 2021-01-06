ea057c6e9d75f8819cf562ad3fe74f192842ac28
NookieGrey <gaguliyanugzar@gmail.com>
2021-01-06 17:47:45 +0400

    img carcase, first try

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import data from "./data.json";
+import {images} from './img';
+
+type IData = number[][][][];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            {(data as IData).map((tRy, index) => (
+                <div className="try">
+                    {tRy.map((block, index) => (
+                        <div key={index} className="block">
+                            {block.map((arr, index) => (
+                                <div key={index} className="row">
+                                    {arr.map((index, key) => <Cell key={key} index={index}/>)}
+                                </div>
+                            ))}
+                        </div>
+                    ))}
+                </div>
+            ))}
+        </div>
+    );
+}
+
+
+interface ICell {
+    index: number
+}
+
+function Cell(props: ICell) {
+    return (
+        <img src={images[props.index]} alt=""/>
+    )
 }
 
 export default App;
---
+++ src/img/index.ts
@@ -0,0 +1,19 @@
+import box from "./box.png";
+import box_placed from "./box_placed.png";
+import grass from "./grass.png";
+import place from "./place.png";
+import stone from "./stone.png";
+import tree from "./tree.png";
+import user from "./user.png";
+import user_paced from "./user_paced.png";
+
+export const images = [
+    tree,
+    stone,
+    grass,
+    user,
+    box,
+    box_placed,
+    place,
+    user_paced,
+]
